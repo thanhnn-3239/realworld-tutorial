ff5cd9b31ffec8b2dc5b67d5a7b77391945c001c
thanhnn-3239 <nguyen.ngoc.thanh@sun-asterick.com>
2026-09-17 10:04:44 +0700

    style: format code for consistency in pending provider link constants and provider link token service

--- src/auth/account-linking/provider-link-token.service.ts
+++ src/auth/account-linking/provider-link-token.service.ts
@@ -6,7 +6,9 @@ import { ProviderLinkToken } from './interfaces/provider-link-token.interface';
 @Injectable()
 export class ProviderLinkTokenService {
   issue(): ProviderLinkToken {
-    const rawToken = randomBytes(PROVIDER_LINK_TOKEN_BYTE_LENGTH).toString('base64url');
+    const rawToken = randomBytes(PROVIDER_LINK_TOKEN_BYTE_LENGTH).toString(
+      'base64url',
+    );
     const tokenHash = this.hash(rawToken);
 
     return { rawToken, tokenHash };
